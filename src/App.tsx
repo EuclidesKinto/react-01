bc8aa0109cafac0f1ddda2e8f10dee176b05092b
Euclides <euclideskinto@gmail.com>
2022-01-18 20:49:40 -0300

    get api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const loadMovies=()=>{
+    fetch('https://api.b7web.com.br/cinema/')
+      .then((res)=>{
+        return res.json();
+      })
+      .then((json)=>{
+        setMovies(json);
+      });
+  }
+
   return (
     <>
-    <h1 className="text-3xl font-bold underline text-violet-800">
-      Hello world!
-    </h1>
+    <div className='flex flex-col'>
+      <div>
+        <button className='block m-5 py-2 px-3 bg-blue-400 rounded text-white'
+        onClick={loadMovies}>Carregar Filmes</button>
+      </div>
+      <p className='m-5 text-gray-700'>Total de Filmes: {movies.length}</p>
+    </div>
+    
     </>
   );
 }
 
 export default App;
+
+// https://api.b7web.com.br/cinema/
