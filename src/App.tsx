c6e0714a5b8cf19f7dd57272ea4d02d0da6c19ab
Euclides <euclideskinto@gmail.com>
2022-01-18 21:07:03 -0300

    get movies with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import {Movie} from './types/Movies';
 
 function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    loadMovies();
+  }, [])
 
   const loadMovies=()=>{
     fetch('https://api.b7web.com.br/cinema/')
@@ -15,16 +20,22 @@ function App() {
   }
 
   return (
-    <>
-    <div className='flex flex-col'>
-      <div>
+    <div className='container mx-auto'>
+      {/* <div>
         <button className='block m-5 py-2 px-3 bg-blue-400 rounded text-white'
         onClick={loadMovies}>Carregar Filmes</button>
-      </div>
-      <p className='m-5 text-gray-700'>Total de Filmes: {movies.length}</p>
+      </div> */}
+      <p className=' text-gray-700 my-5'>Total de Filmes: {movies.length}</p>
+    <div className='grid grid-cols-6 gap-3'>
+      {movies.map((item, index)=>(
+        <div  key={index}>
+          <img className='w-32 block my-2' src={item.avatar} alt={item.titulo} />
+          <p>{item.titulo}</p>
+        </div>
+      ))}
     </div>
     
-    </>
+    </div>
   );
 }
 
