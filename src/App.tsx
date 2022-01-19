082a1fd6d76bd9e7b40a8cde5f92fafbb41f7154
Euclides <euclideskinto@gmail.com>
2022-01-18 21:46:28 -0300

    update  no layout css

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import {Movie} from './types/Movies';
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
 
-  useEffect(() => {
-    loadMovies();
-  }, [])
+  // useEffect(() => {
+  //   loadMovies();
+  // }, [])
 
   const loadMovies=()=>{
     fetch('https://api.b7web.com.br/cinema/')
@@ -21,20 +21,25 @@ function App() {
 
   return (
     <div className='container mx-auto'>
-      {/* <div>
-        <button className='block m-5 py-2 px-3 bg-blue-400 rounded text-white'
-        onClick={loadMovies}>Carregar Filmes</button>
-      </div> */}
-      <p className=' text-gray-700 my-5'>Total de Filmes: {movies.length}</p>
-    <div className='grid grid-cols-6 gap-3'>
-      {movies.map((item, index)=>(
-        <div  key={index}>
-          <img className='w-32 block my-2' src={item.avatar} alt={item.titulo} />
-          <p>{item.titulo}</p>
+      <div className="flex flex-col text-center w-full mb-5 border-b-2">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font my-4 text-gray-900">Filmes em Cartaz</h1>
+        <div className='my-5'>
+            <button className='py-2 px-3 bg-blue-400 rounded text-white'
+            onClick={loadMovies}>Carregar Filmes</button>
         </div>
-      ))}
-    </div>
-    
+        <p className="lg:w-2/3 mx-auto leading-relaxed text-base mb-2">Total de Filmes: {movies.length}</p>
+      </div>
+      
+      <div className='grid grid-cols-6 gap-3'>
+        {movies.map((item, index)=>(
+          <div  key={index}>
+            <img className='w-32 block my-2' src={item.avatar} alt={item.titulo} />
+            <div className="mt-2">
+              <h2 className="text-gray-900 text-base">{item.titulo}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
